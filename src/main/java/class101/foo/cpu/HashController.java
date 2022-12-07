3ca911365fb65f0ab59076a532cf0c0906313a3d
choiseon <com1368@naver.com>
2022-12-08 01:43:40 +0900

    change message

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello";
+        return "seonkang";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
